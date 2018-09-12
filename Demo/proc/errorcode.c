cfbb303b4933b01319db41950dbb5dd4c3debced
zhyingkun <1464959348@qq.com>
2018-09-12 14:21:22 +0800

    add some comment

--- Demo/proc/errorcode.c
+++ Demo/proc/errorcode.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 __attribute((constructor)) void before_main()
 {
     printf("%s\n", __FUNCTION__);
@@ -7,12 +8,15 @@ __attribute((destructor)) void after_main()
 {
     printf("%s\n", __FUNCTION__);
 }
+
 int main(int argc, char *argv[])
 {
+    printf("Start Main \n");
     // printf("argv[2000]: %p\n", argv[2000]);
     int a;
     printf("sizeof a : %lu\n", sizeof(a));
-    char array[8 * 1024 * 1024 - 1024 * 3 - 64 - 7];
-    printf("sizeof a : %lu\n", sizeof(array));
+    //This local var alloc will cause segament fault, the stack overflow
+//    char array[8 * 1024 * 1024 - 1024 * 3 - 64 - 7];
+//    printf("sizeof a : %lu\n", sizeof(array));
     return 0;
 }
