2317193130156549105443dbf7f646657bb255ec
zhyingkun <1464959348@qq.com>
2019-03-15 11:21:51 +0800

    Complete cdecl codes

--- ExpertCProgramming/cdecl/cdecl.c
+++ ExpertCProgramming/cdecl/cdecl.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-
+#include <stdlib.h>
 #define MAXTOKENS 100
 #define MAXTOKENLEN 64
 
@@ -44,6 +44,81 @@ void gettoken(){
 	char* p = this.string;
 	while((*p = getchar()) == ' ');
 	if(isalnum(*p)){
-		
+		while(isalnum(*++p = getchar()));
+		ungetc(*p, stdin);
+		*p = '\0';
+		this.type = classify_string();
+		return;
+	}
+	if(*p == '*'){
+		strcpy(this.string, "pointer to");
+		this.type = '*';
+		return;
+	}
+	this.string[1] = '\0';
+	this.type = *p;
+	return;
+}
+
+void read_to_first_identifier(){
+	gettoken();
+	while(this.type != IDENTIFIER){
+		push(this);
+		gettoken();
+	}
+	printf("%s is ", this.string);
+	gettoken();
+}
+
+void deal_with_arrays(){
+	while(this.type == '['){
+		printf("array ");
+		gettoken();
+		if(isdigit(this.string[0])){
+			printf("0..%d ", atoi(this.string)-1);
+			gettoken();
+		}
+		gettoken();
+		printf("of ");
 	}
 }
+
+void deal_with_function_args(){
+	while(this.type != ')'){
+		gettoken();
+	}
+	gettoken();
+	printf("function returning ");
+}
+
+void deal_with_pointers(){
+	while(stack[top].type == '*'){
+		printf("%s ", pop.string);
+	}
+}
+
+void deal_with_declarator(){
+	switch(this.type){
+		case '[': deal_with_arrays(); break;
+		case '(': deal_with_function_args();break;
+	}
+	deal_with_pointers();
+	while(top >= 0){
+		if(stack[top].type == '('){
+			pop;
+			gettoken();
+			deal_with_declarator();
+		}else{
+			printf("%s ", pop.string);
+		}
+	}
+}
+
+int main(int argc, char* argv[]){
+	read_to_first_identifier();
+	deal_with_declarator();
+	printf("\n");
+	return 0;
+}
+
+// Test Example(Input from stdin): char* (*c[10])(int **p);
