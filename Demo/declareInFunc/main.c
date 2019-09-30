0c5278c25ed233a287a7e78ed7c3308d4c6151a8
zhyingkun <1464959348@qq.com>
2019-09-30 22:12:33 +0800

    C declare in function

---
+++ Demo/declareInFunc/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+	void printHello();
+	printHello();
+	void printNice();
+	printNice();
+	return 0;
+}
+
+void printNice(){ printf("Nice\n"); }//printHelo(); }
+void printHello(){ printf("Hello\n"); }
+
