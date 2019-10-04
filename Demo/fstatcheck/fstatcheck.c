2f69d96245c438efcc725adb0efcdf693b0e029c
zhyingkun <1464959348@qq.com>
2019-10-04 21:11:18 +0800

    Format demo fstatcheck

--- Demo/fstatcheck/fstatcheck.c
+++ Demo/fstatcheck/fstatcheck.c
@@ -3,30 +3,29 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 
-int main (int argc, char **argv) 
-{
-	struct stat stat;
-	char *type, *readok;
+int main(int argc, char** argv) {
+  struct stat stat;
+  char *type, *readok;
 
-	/* $end statcheck */
-	if (argc != 2) {
-		fprintf(stderr, "usage: %s <fd>\n", argv[0]);
-		exit(0);
-	}
-	/* $begin statcheck */
-	fstat(atoi(argv[1]), &stat);
-	if (S_ISREG(stat.st_mode))     /* Determine file type */
-		type = "regular";
-	else if (S_ISDIR(stat.st_mode))
-		type = "directory";
-	else 
-		type = "other";
-	if ((stat.st_mode & S_IRUSR)) /* Check read access */
-		readok = "yes";
-	else
-		readok = "no";
+  /* $end statcheck */
+  if (argc != 2) {
+    fprintf(stderr, "usage: %s <fd>\n", argv[0]);
+    exit(0);
+  }
+  /* $begin statcheck */
+  fstat(atoi(argv[1]), &stat);
+  if (S_ISREG(stat.st_mode)) /* Determine file type */
+    type = "regular";
+  else if (S_ISDIR(stat.st_mode))
+    type = "directory";
+  else
+    type = "other";
+  if ((stat.st_mode & S_IRUSR)) /* Check read access */
+    readok = "yes";
+  else
+    readok = "no";
 
-	printf("type: %s, read: %s\n", type, readok);
-	exit(0);
+  printf("type: %s, read: %s\n", type, readok);
+  exit(0);
 }
 /* $end fstatcheck */
--- Demo/fstatcheck/fstatcheck_wrap.c
+++ Demo/fstatcheck/fstatcheck_wrap.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main(int argc, char* argv[]){
-	if(argc != 2){
-		fprintf(stderr, "usage: %s <fd>\n", argv[0]);
-		exit(0);
-	}
-	dup2(STDIN_FILENO, atoi(argv[1]));
-	execve("fstatcheck", argv, NULL);
-	return 0;
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    fprintf(stderr, "usage: %s <fd>\n", argv[0]);
+    exit(0);
+  }
+  dup2(STDIN_FILENO, atoi(argv[1]));
+  execve("fstatcheck", argv, NULL);
+  return 0;
 }
