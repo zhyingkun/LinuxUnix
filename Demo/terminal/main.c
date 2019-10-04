13047e25a7a3084c1fc78664e70a3a7ca3a3e238
zhyingkun <1464959348@qq.com>
2019-10-04 21:11:50 +0800

    Update demo terminal

--- Demo/terminal/main.c
+++ Demo/terminal/main.c
@@ -2,29 +2,29 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdio.h>
-int main(int argc, char* argv[])
-{
-	
-	if(isatty(0)){
-		printf("stdin is tty\n");	
-	}else{
-		printf("stdin is not tty\n");
-	}
-	if(isatty(1)){
-		printf("stdout is tty\n");	
-	}else{
-		printf("stdout is not tty\n");	
-	}
+int main(int argc, char* argv[]) {
 
-	struct stat buf;
-	fstat(0, &buf);
-	printf("stdin: %d\n", major(buf.st_rdev));
-	fstat(1, &buf);
-	printf("stdout: %d\n", major(buf.st_rdev));
-	fflush(NULL);
-	close(0);
-	close(1);
-	close(2);
-	for(;;);
-	return 0;
+  if (isatty(0)) {
+    printf("stdin is tty\n");
+  } else {
+    printf("stdin is not tty\n");
+  }
+  if (isatty(1)) {
+    printf("stdout is tty\n");
+  } else {
+    printf("stdout is not tty\n");
+  }
+
+  struct stat buf;
+  fstat(0, &buf);
+  printf("stdin: %d\n", major(buf.st_rdev));
+  fstat(1, &buf);
+  printf("stdout: %d\n", major(buf.st_rdev));
+  fflush(NULL);
+  close(0);
+  close(1);
+  close(2);
+  for (;;)
+    ;
+  return 0;
 }
